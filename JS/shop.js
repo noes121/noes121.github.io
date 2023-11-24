927980d6e3355c150b90153d931d459e8f0100ae
ORX <107448380+noes121@users.noreply.github.com>
2023-11-24 19:58:51 -0400

    Add files via upload

--- JS/shop.js
+++ JS/shop.js
@@ -211,5 +211,8 @@ function actualizarTotalCarrito(){
     total = Math.round(total * 100)/100;
 
     document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
-document.getElementsByClassName('totalpagar')[0].innerText = '$'+total.toLocaleString("es") + ",00";
+    document.getElementsByClassName('totalpagar')[0].innerText = '$'+total.toLocaleString("es") + ",00";
+     
+
+    
 }
